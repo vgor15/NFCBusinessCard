f1a4b940b04e4f81214f78f322ef8876f5802af2
Vyom Gor <gorvyom9@gmail.com>
2023-10-14 21:49:58 +0530

    Theme

--- app/src/main/java/com/vyomgor/nfc/MainActivity.java
+++ app/src/main/java/com/vyomgor/nfc/MainActivity.java
@@ -40,7 +40,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textView=(TextView) findViewById(R.id.text);
-        inputView=(EditText) findViewById(R.id.input);
+        inputView=(EditText) findViewById(R.id.inputText);
 
 
         try {
